3662d37c859e780eeeeb6ae3468008143e1073df
jjohnsonpsu <72718037+jjohnsonpsu@users.noreply.github.com>
2021-03-15 19:42:05 -0400

    19.2

--- 19.2-activities/01_Conditionals/unsolved/js/index.js
+++ 19.2-activities/01_Conditionals/unsolved/js/index.js
@@ -1,3 +1,18 @@
 $('.tab').on("click", function(){
   
+    if ($(this).hasClass("tab1")) {
+        $(".tab1").addClass("active");
+        $(".tab1").removeClass("active");
+        $("#tabcontent1").css("display", "block");
+        $("#tabcontent2").css("display", "none");
+    }
+
+    else if ($(this).hasClass("tab2")) {
+        $(".tab2").addClass("active");
+        $(".tab1").removeClass("active");
+        $("#tabcontent2").css("display", "block");
+        $("#tabcontent1").css("display", "none");
+    }
+
+
 });
